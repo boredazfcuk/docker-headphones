d954d06d69542f7c80af6dfecac1bde06852a226
boredazfcuk <boredazfcuk@hotmail.co.uk>
2019-10-29 12:11:07 +0000

    Scripted push of stack containers

--- start-headphones.sh
+++ start-headphones.sh
@@ -2,16 +2,16 @@
 
 ##### Functions #####
 Initialise(){
+   LANIP="$(hostname -i)"
    echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    ***** Starting rembo10/headphones container *****"
-
    if [ -z "${USER}" ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') WARNING: User name not set, defaulting to 'user'"; USER="user"; fi
    if [ -z "${UID}" ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') WARNING: User ID not set, defaulting to '1000'"; UID="1000"; fi
    if [ -z "${GROUP}" ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') WARNING: Group name not set, defaulting to 'group'"; GROUP="group"; fi
    if [ -z "${GID}" ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') WARNING: Group ID not set, defaulting to '1000'"; GID="1000"; fi
-
    echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Local user: ${USER}:${UID}"
    echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Local group: ${GROUP}:${GID}"
    echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Headphones application directory: ${APPBASE}"
+   sed -i "s%http_host =.*$%http_host = ${LANIP}%" "${CONFIGDIR}/headphones.ini"  
 }
 
 CreateGroup(){
@@ -36,6 +36,8 @@ CreateUser(){
 
 SetOwnerAndGroup(){
    echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Correct owner and group of application files, if required"
+   find "${CONFIGDIR}" ! -user "${USER}" -exec chown "${USER}" {} \;
+   find "${CONFIGDIR}" ! -group "${GROUP}" -exec chgrp "${GROUP}" {} \;
    find "${APPBASE}" ! -user "${USER}" -exec chown "${USER}" {} \;
    find "${APPBASE}" ! -group "${GROUP}" -exec chgrp "${GROUP}" {} \;
 }
@@ -49,4 +51,5 @@ LaunchHeadphones(){
 Initialise
 CreateGroup
 CreateUser
+SetOwnerAndGroup
 LaunchHeadphones
