7d3168dcc20b72482fba72ee21e734be9fbb3d93
boredazfcuk <boredazfcuk@hotmail.co.uk>
2019-11-02 18:48:50 +0000

    Manual push of any changes to stack containers

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,3 +1,3 @@
 #!/bin/ash
-wget --quiet --tries=1 --spider "https://${HOSTNAME}:8181/headphones/home" --no-check-certificate || exit 1
+wget --quiet --tries=1 --no-check-certificate --spider "https://${HOSTNAME}:8181/headphones/home" || exit 1
 exit 0
