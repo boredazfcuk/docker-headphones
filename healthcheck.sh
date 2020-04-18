7b34547cb5601f56d4edbcdf5ea11b38fb49d3bb
boredazfcuk <boredazfcuk@hotmail.co.uk>
2020-04-18 23:16:10 +0100

    Please check change.log

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,6 +1,6 @@
 #!/bin/ash
 
-if [ "$(nc -z "$(hostname -i)" 8181; echo $?)" -ne 0 ]; then
+if [ "$(netstat -plnt | grep -c 8181)" -ne 1 ]; then
    echo "Headphones WebUI not responding on port 8181"
    exit 1
 fi
